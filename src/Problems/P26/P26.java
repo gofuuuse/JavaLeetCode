fc680eb32474e7d725a21bf8a6cddfb70508429d
gofuuse <nihad.mammadov.16@gmail.com>
2025-08-08 22:02:22 +0400

    Problem 26 SOLVED

---
+++ src/Problems/P26/P26.java
@@ -0,0 +1,17 @@
+package Problems.P26;
+
+public class P26 {
+    public int removeDuplicates(int[] nums) {
+        int k = 1;
+        for(int i = 1 ; i<nums.length;i++){
+            if(nums[i] != nums[i-1]){
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+
+
+        return k;
+    }
+}
