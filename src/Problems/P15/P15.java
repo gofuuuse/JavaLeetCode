24b276b949e40cca27432ed34d4946bf545a28bf
gofuuse <nihad.mammadov.16@gmail.com>
2025-08-08 13:31:48 +0400

    Problem 15 SOLVED

---
+++ src/Problems/P15/P15.java
@@ -0,0 +1,47 @@
+package Problems.P15;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class P15 {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for(int i = 0;i<nums.length;i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int left = i + 1;
+            int right = nums.length - 1;
+            while(left<right) {
+                int total = nums[i] + nums[left] + nums[right];
+                if (total == 0) {
+                    List<Integer> ls = new ArrayList<>(List.of(nums[i],nums[left],nums[right]));
+                    Collections.sort(ls);
+                    result.add(ls);
+
+
+
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+
+                    left++;
+                    right--;
+
+                } else if (total < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+
+
+
+            }
+        }
+
+        return result;
+
+    }
+}
